5a190e00de10fda8547dfc67b0af814af21d9a9d
joniitown <jonathan.weber08@outlook.de>
2025-10-25 18:48:28 +0200

    will make to repo to use an actual 3d engine not this wolfenstein like pseudo 3d

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,19 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.jmonkeyengine:jme3-core:3.8.0-stable")
+    implementation("org.jmonkeyengine:jme3-desktop:3.8.0-stable")
+    implementation("org.jmonkeyengine:jme3-lwjgl3:3.8.0-stable")
+    implementation("org.jmonkeyengine:jme3-plugins:3.8.0-stable")
+   // implementation("org.jmonkeyengine:jme3-bullet:3.8.0-stable")
+// optional
 }
 
 tasks.test {
